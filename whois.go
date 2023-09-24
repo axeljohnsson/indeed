9d0b6d298f9ca2311d1e3c0f3bff270f2adf5cd6
Axel Johnsson <johnsson.axel@gmail.com>
2023-09-24 10:02:43 +0200

    Add a WHOIS client

---
+++ whois.go
@@ -0,0 +1,129 @@
+package indeed
+
+import (
+	"context"
+	"errors"
+	"io"
+	"net/textproto"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var (
+	errNoServer = errors.New("no WHOIS server")
+	tldRE       = regexp.MustCompile(`\.[a-z]+$`)
+	updateRE    = regexp.MustCompile(`\S+Z\S*`)
+)
+
+type WHOISClient struct {
+	m func(string) string
+}
+
+func NewWHOISClient() *WHOISClient {
+	return &WHOISClient{
+		m: func(name string) string {
+			switch tldRE.FindString(name) {
+			case ".io":
+				return "whois.nic.io:43"
+			default:
+				return ""
+			}
+		},
+	}
+}
+
+func (c *WHOISClient) LookupDomain(ctx context.Context, name string) (*RDAPDomain, error) {
+	addr := c.m(name)
+	if addr == "" {
+		return nil, errNoServer
+	}
+
+	conn, err := textproto.Dial("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	id, err := conn.Cmd(name)
+	if err != nil {
+		return nil, err
+	}
+
+	conn.StartResponse(id)
+	defer conn.EndResponse(id)
+
+	return c.unmarshal(conn)
+}
+
+func (c *WHOISClient) unmarshal(conn *textproto.Conn) (*RDAPDomain, error) {
+	domain := &RDAPDomain{
+		Events: make([]RDAPEvent, 0),
+	}
+
+	for {
+		line, err := conn.ReadLine()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return nil, err
+		}
+
+		before, after, found := c.cutTrim(line)
+		if !found {
+			break
+		}
+
+		switch before {
+		case "Domain Name":
+			domain.Name = after
+		case "Creation Date":
+			if err := c.unmarshalEvent(after, domain, "registration"); err != nil {
+				return nil, err
+			}
+		case "Registry Expiry Date":
+			if err := c.unmarshalEvent(after, domain, "expiration"); err != nil {
+				return nil, err
+			}
+		case "Updated Date":
+			if err := c.unmarshalEvent(after, domain, "last changed"); err != nil {
+				return nil, err
+			}
+		}
+
+		if strings.HasPrefix(before, ">>>") {
+			data := updateRE.FindString(after)
+			action := "last update of WHOIS database"
+			if err := c.unmarshalEvent(data, domain, action); err != nil {
+				return nil, err
+			}
+			break
+		}
+	}
+
+	if len(domain.Events) == 0 {
+		return nil, nil
+	}
+
+	return domain, nil
+}
+
+func (c *WHOISClient) unmarshalEvent(data string, domain *RDAPDomain, action string) error {
+	t, err := time.Parse(time.RFC3339, data)
+	if err != nil {
+		return err
+	}
+	domain.Events = append(domain.Events, RDAPEvent{
+		Action: action,
+		Date:   t,
+	})
+	return nil
+}
+
+func (c *WHOISClient) cutTrim(line string) (string, string, bool) {
+	b, a, ok := strings.Cut(line, ":")
+	b = strings.TrimSpace(b)
+	a = strings.TrimSpace(a)
+	return b, a, ok
+}
---
+++ whois_test.go
@@ -0,0 +1,137 @@
+package indeed
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"io"
+	"net"
+	"net/textproto"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+func whoisServer(ch chan<- net.Addr) error {
+	l, err := net.Listen("tcp", ":")
+	if err != nil {
+		return err
+	}
+	defer l.Close()
+
+	ch <- l.Addr()
+
+	conn, err := l.Accept()
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	r := textproto.NewReader(bufio.NewReader(conn))
+	line, err := r.ReadLine()
+	if err != nil {
+		return err
+	}
+
+	var name string
+	switch line {
+	case "example.com":
+		name = "testdata/whois-example-com.txt"
+	case "example.net":
+		name = "testdata/whois-example-net.txt"
+	case "example.org":
+		name = "testdata/whois-example-org.txt"
+	default:
+		_, err := fmt.Fprint(conn, "Domain not found.\r\n")
+		return err
+	}
+
+	f, err := os.Open(name)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	if _, err := io.Copy(conn, f); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestWHOIS(t *testing.T) {
+	tests := []struct {
+		description string
+		name        string
+		want        *RDAPDomain
+	}{
+		{
+			"ok",
+			"example.com",
+			&RDAPDomain{
+				Name: "EXAMPLE.COM",
+				Events: []RDAPEvent{
+					{
+						Action: "last changed",
+						Date:   time.Date(2023, 8, 14, 7, 1, 38, 0, time.UTC),
+					},
+					{
+						Action: "registration",
+						Date:   time.Date(1995, 8, 14, 4, 0, 0, 0, time.UTC),
+					},
+					{
+						Action: "expiration",
+						Date:   time.Date(2024, 8, 13, 4, 0, 0, 0, time.UTC),
+					},
+					{
+						Action: "last update of WHOIS database",
+						Date:   time.Date(2023, 9, 6, 11, 4, 43, 0, time.UTC),
+					},
+				},
+			},
+		},
+		{
+			"not found",
+			"404.com",
+			nil,
+		},
+	}
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.description, func(t *testing.T) {
+			g, ctx := errgroup.WithContext(context.Background())
+			ch := make(chan net.Addr)
+
+			g.Go(func() error {
+				return whoisServer(ch)
+			})
+
+			g.Go(func() error {
+				addr := <-ch
+
+				c := &WHOISClient{
+					m: func(string) string {
+						return addr.String()
+					},
+				}
+				got, err := c.LookupDomain(ctx, tc.name)
+				if err != nil {
+					return err
+				}
+
+				if !reflect.DeepEqual(got, tc.want) {
+					return fmt.Errorf("got: %v; want: %v", got, tc.want)
+				}
+
+				return nil
+			})
+
+			if err := g.Wait(); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
